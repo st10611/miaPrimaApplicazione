fe671d1dcceccae5852ff2dfd4fc8a997ed8a324
Matcaboja Marcus Angelo <st10611@ITIS.JESI>
2025-02-21 10:54:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/miaprimaapplicazione/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.miaprimaapplicazione;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //trova il bottone nel layout
+        Button button = findViewById(R.id.button);
+
+        //Assegna un listener al bottone
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/miaprimaapplicazione/SecondActivity.java
@@ -0,0 +1,15 @@
+package com.example.miaprimaapplicazione;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //visualizzare la grafica del file .xml
+        setContentView(R.layout.activity_second);
+    }
+}
