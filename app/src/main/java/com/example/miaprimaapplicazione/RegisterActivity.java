650332450798754b5b92a1abd0a1224588960700
st10611 <st10611@iismarconipieralisi.it>
2025-03-07 10:13:34 +0100

    modifica main activity

--- app/src/main/java/com/example/miaprimaapplicazione/RegisterActivity.java
+++ app/src/main/java/com/example/miaprimaapplicazione/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.miaprimaapplicazione;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -11,5 +12,9 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         //visualizzare la grafica del file .xml
         setContentView(R.layout.activity_register);
+        //collegamento con elementi grafici
+        EditText none = findViewById(R.id.txt_Nome);
     }
+
+
 }
